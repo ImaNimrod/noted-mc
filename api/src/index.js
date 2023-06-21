c7a2f93f83220c4b45f88d6c615e8f378629933e
nimrod <nun.ya@business.com>
2023-06-21 13:48:20 -0400

    api: fixed outdated log message

--- api/src/index.js
+++ api/src/index.js
@@ -37,5 +37,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-    console.log("noted-mc server started at: http://localhost:" + port);
+    console.log("noted-mc api instance started at on port: " + port);
 });
