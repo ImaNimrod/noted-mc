209ba5f4dc229feb6070d75120623a7e2a280858
nimrod <nun.ya@business.com>
2023-07-09 23:13:14 -0400

    client: verison bump

--- client/src/main/java/net/nimrod/noted/Noted.java
+++ client/src/main/java/net/nimrod/noted/Noted.java
@@ -13,7 +13,7 @@ import org.lwjgl.glfw.GLFW;
 public class Noted implements ModInitializer {
 
     public static final String NAME = "noted";
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
     public static final String AUTHOR = "nimrod";
 
     public static final Noted INSTANCE = new Noted();
