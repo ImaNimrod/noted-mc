cbe55a9e12065336b02a50daf69b036857b8ff00
nimrod <nun.ya@business.com>
2025-10-11 10:58:44 -0400

    add open song folder button

--- src/main/java/nimrod/noted/gui/ConfigScreen.java
+++ src/main/java/nimrod/noted/gui/ConfigScreen.java
@@ -2,8 +2,10 @@ package nimrod.noted.gui;
 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.CheckboxWidget;
 import net.minecraft.text.Text;
+import net.minecraft.util.Util;
 import nimrod.noted.Noted;
 
 public class ConfigScreen extends Screen {
@@ -21,6 +23,12 @@ public class ConfigScreen extends Screen {
             .checked(Noted.CONFIG.swingHand)
             .maxWidth(120)
             .pos(width / 2 - 40, 45)
+            .build()
+        );
+
+        addDrawableChild(
+            ButtonWidget.builder(Text.literal("Open folder"), button -> Util.getOperatingSystem().open(Noted.FOLDER))
+            .dimensions(width / 2 - 50, 75, 100, 20)
             .build()
         );
     }
